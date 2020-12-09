5b780aeb9c956d59373ae7ef29212ab02e0c39b1
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-09 12:37:45 -0500

    lecture work-in-progress

--- middleware-example/src/index.js
+++ middleware-example/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import rootReducer from './reducers';
+import logger from 'redux-logger';
 
 import Title from './components/Title';
 import FriendsList from './components/FriendsList';
