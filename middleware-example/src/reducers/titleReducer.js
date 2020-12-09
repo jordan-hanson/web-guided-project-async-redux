c39ae08ccbc317009d32d78942a1c2984b192b96
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-09 12:35:24 -0500

    lecture work-in-progress

--- middleware-example/src/reducers/titleReducer.js
+++ middleware-example/src/reducers/titleReducer.js
@@ -5,6 +5,8 @@ const initialState = {
 };
 
 export function titleReducer(state = initialState, action) {
+  console.log("title state: ", state);
+  console.log("title action", action);
   switch (action.type) {
     case UPDATE_TITLE:
       return {
