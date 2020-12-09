7994892729032bd4edf3e9cc5f7e16ca9a550757
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-09 13:09:44 -0500

    lecture work-in-progress

--- thunk/src/actions/index.js
+++ thunk/src/actions/index.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
 
+export const FETCHING_QUOTE_START = 'FETCHING_QUOTE_START';
+export const FETCHING_QUOTE_SUCCESS = 'FETCHING_QUOTE_SUCCESS';
+export const FETCHING_QUOTE_FAIL = 'FETCHING_QUOTE_FAIL';
+
 export const getQuote = () => dispatch => {
     // 1. set isFetching = true;
     // 2. setoff our api call
     // 3. if our api call is successful, add result to quote
     // 4. if our api call is unsuccessful, add error message to error
-    dispatch({type:'FETCHING_QUOTE_START'});
+    dispatch({type:FETCHING_QUOTE_START});
 
     setTimeout(()=>{
-        dispatch({type:'FETCHING_QUOTE_SUCCESS', payload:"THIS IS A NEW QUOTE"});
+        dispatch({type:FETCHING_QUOTE_SUCCESS, payload:"THIS IS A NEW QUOTE"});
     }, 3000);    
 }
