c84e4451e47e2766d109cc7556bce28e3d54af65
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-09 13:22:58 -0500

    lecture work-in-progress

--- thunk/src/actions/index.js
+++ thunk/src/actions/index.js
@@ -10,17 +10,16 @@ export const getQuote = () => dispatch => {
     // 3. if our api call is successful, add result to quote
     // 4. if our api call is unsuccessful, add error message to error
     dispatch({type:FETCHING_QUOTE_START});
+    
+    axios
+        .get('https://api.kanye.rest')
+        .then(res=> {
+            dispatch({type:FETCHING_QUOTE_SUCCESS, payload:res.data.quote});
+        })
+        .catch(err => {
+            dispatch({type:FETCHING_QUOTE_FAIL, payload: err.response.message});
+        });
 
-    setTimeout(()=> {
-        axios
-            .get('https://api.kanye.rest')
-            .then(res=> {
-                dispatch({type:FETCHING_QUOTE_SUCCESS, payload:res.data.quote});
-            })
-            .catch(err => {
-                dispatch({type:FETCHING_QUOTE_FAIL, payload: err.response.message});
-            });
-    }, 3000); 
 }
 
 const setName = (name) => {
