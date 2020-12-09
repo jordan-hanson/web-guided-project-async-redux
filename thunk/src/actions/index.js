451473644cd34daaaee0a0ec6a9588d991bfb1c5
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-09 12:54:35 -0500

    lecture work-in-progress

--- thunk/src/actions/index.js
+++ thunk/src/actions/index.js
@@ -1,4 +1,7 @@
 
 export const getQuote = () => {
-    
+    // 1. set isFetching = true;
+    // 2. setoff our api call
+    // 3. if our api call is successful, add result to quote
+    // 4. if our api call is unsuccessful, add error message to error
 }
