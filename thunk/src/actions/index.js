21e4ea1d3c971b1feed5b94274418163d4085d66
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-09 13:24:34 -0500

    lecture work-in-progress

--- thunk/src/actions/index.js
+++ thunk/src/actions/index.js
@@ -12,7 +12,7 @@ export const getQuote = () => dispatch => {
     dispatch({type:FETCHING_QUOTE_START});
 
     axios
-        .get('https://api.kanye.rest')
+        .get(`https://api.kanye.rest`)
         .then(res=> {
             dispatch({type:FETCHING_QUOTE_SUCCESS, payload:res.data.quote});
         })
